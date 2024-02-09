7a49f297bf2b0dbd7f935c12f80761de7f5ea8c9
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2024-02-10 01:05:20 +0530

    Databse addaed

---
+++ temp.py
@@ -0,0 +1,15 @@
+import requests
+from json import JSONEncoder
+# from xml import XMLEncoder
+url="https://api.indiankanoon.org/search/?formInput=smuggling&maxpages=100&pagenum=600"
+api = "2852f54424a360a69edc0feebedf8b8a969e93b3"
+headers={
+    "Authorization": "Token " + api,
+    "format": "json"
+}
+response = requests.post(url, headers=headers)
+if response.status_code == 200:
+    data = response.json()  # Assuming API returns JSON
+    print(JSONEncoder().encode(data))
+else:
+    print("Request failed with status code:", response.status_code)
