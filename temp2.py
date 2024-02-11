2c27d06de4b0f952c5a90c8af95a15abb2bf27fe
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2024-02-12 02:34:17 +0530

    TEMP@

--- temp2.py
+++ temp2.py
@@ -9,13 +9,36 @@ firebase_admin.initialize_app(cred)
 db=firestore.client()
 
 xz="murder"
-file=f"{xz}"+"_laws.json"
-with open(f"./database/{xz}"+"_laws.json") as f1:
+with open(f"./database/{xz}"+"_cases.json") as f1:
     data=json.load(f1)
-    # print(data)
-    dict = {}
+    # print(len(data))
+    dict1 = {}
+    dict2 = {}
+    dict3 = {}
+    dict4 = {}
+    dict5 = {}
+    l=[dict1, dict2, dict3, dict4, dict5]
     for i in range(0,len(data)):
-        dict[str(i)]=data[i]
-    # print(dict)
-    doc= db.collection("laws").document(f"{xz}")
-    doc.set(dict)
+        # print(i)
+        if i<900:
+            dict1[str(i)]=data[i]    
+        elif i<1800:
+            dict2[str(i)]=data[i]
+        elif i<2700:
+            dict3[str(i)]=data[i]
+        elif i<3600:
+            dict4[str(i)]=data[i]
+        else:
+            dict5[str(i)]=data[i]
+    doc= db.collection("cases").document(f"{xz}1")
+    doc.set(dict1)
+    doc2= db.collection("cases").document(f"{xz}2")
+    doc2.set(dict2)
+    doc3= db.collection("cases").document(f"{xz}3")
+    doc3.set(dict3)
+    doc4= db.collection("cases").document(f"{xz}4")
+    doc4.set(dict4)
+    doc5= db.collection("cases").document(f"{xz}5")
+    doc5.set(dict5)
+        # print(doc)
+        # doc.set(l[i])
