34849047f6f8782423b8cacad6ed817233e556a9
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2024-02-12 01:58:17 +0530

    Helping function

---
+++ temp2.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+import json
+
+cred=credentials.Certificate("./firebase-sdk.json")
+firebase_admin.initialize_app(cred)
+
+db=firestore.client()
+
+xz="murder"
+file=f"{xz}"+"_laws.json"
+with open(f"./database/{xz}"+"_laws.json") as f1:
+    data=json.load(f1)
+    # print(data)
+    dict = {}
+    for i in range(0,len(data)):
+        dict[str(i)]=data[i]
+    # print(dict)
+    doc= db.collection("laws").document(f"{xz}")
+    doc.set(dict)
