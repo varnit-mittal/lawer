4bec495febf770901cb929c97a5398da2c70ca7e
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2024-02-14 22:05:25 +0530

    view created

--- lawer/views.py
+++ lawer/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+import firebase_admin
+from firebase_admin import credentials
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+def upload(request,username):
+    user=username
+    if(request.POST or request.FILES):
+        
