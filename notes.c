1a3cf3e345bbdb10f2a9b6600c96dc6826dfba01
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2024-02-16 22:12:55 +0530

    notes_api

---
+++ notes.c
@@ -0,0 +1,11 @@
+/**
+ * @file api
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-14
+ * 
+ * @copyright Copyright (c) 2024
+ *  /folder {post, {"name":folder_name}}/
+ * once signup send request to create a new folder
+ */
