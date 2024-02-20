466d536859f14ac2b6a0aeff31f8ad5a60596809
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2024-02-21 00:58:25 +0530

    init

--- lawer/__init__.py
+++ lawer/__init__.py
@@ -2,8 +2,10 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 import os
-
-cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_SDK")))
+try:
+    cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_SDK")))
+except:
+    cred=credentials.Certificate("./a.json")
 firebase_app = firebase_admin.initialize_app(cred, {
     'storageBucket': 'calcium-backup-411020.appspot.com'
 })
