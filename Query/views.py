373b233edd1f73ed17063c8771803fdf67e9aa83
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2024-02-20 21:11:53 +0530

    update

--- Query/views.py
+++ Query/views.py
@@ -1,49 +1,14 @@
-from django.shortcuts import render
+import os
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser, MultiPartParser 
+from rest_framework import status
+from django.http import FileResponse, HttpResponse, StreamingHttpResponse
+import firebase_admin
+import json
+from firebase_admin import credentials, storage
 
-# Create your views here.
-"""
-At the command line, only need to run once to install the package via pip:
-
-$ pip install google-generativeai
-"""
-
-import google.generativeai as genai
-
-genai.configure(api_key="YOUR_API_KEY")
-
-# Set up the model
-generation_config = {
-  "temperature": 0.9,
-  "top_p": 1,
-  "top_k": 1,
-  "max_output_tokens": 2048,
-}
-
-safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-]
-
-model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
-
-convo = model.start_chat(history=[
-])
-
-convo.send_message("YOUR_USER_INPUT")
-print(convo.last.text)
+cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_SDK")))
+firebase_app = firebase_admin.initialize_app(cred, {
+    'storageBucket': 'calcium-backup-411020.appspot.com'
+})
